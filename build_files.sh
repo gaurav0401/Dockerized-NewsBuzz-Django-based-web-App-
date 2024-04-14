a70ac8cb89fa5789cce0a5143c45c4e98df8aba3
gaurav bhatt <gaurav.tnv6970@gmail.com>
2024-04-14 16:49:30 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
+brew install python@3.9
+conda create -n ENV_NAME python=3.9
+pip3.9 --version
 pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
